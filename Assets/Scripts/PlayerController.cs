9d3bbf18cab648eee66bd899e1eb99a21847a22c
Your Name <gabrieldelvalle000@gmail.com>
2026-03-05 14:27:08 -0700

    Player Movement complete, working on adding boundaries

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Rigidbody2D rb;
+    public float moveSpeed;
+
+    private Vector2 _moveDirection;
+
+    public InputActionReference move;
+
 
     // Update is called once per frame
     void Update()
     { 
-        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime); 
+        
+         //Sees what button is pressed and applies it to moveDirection
+         //Remember to hook up in the inspector 
+        _moveDirection = move.action.ReadValue<Vector2>();
+        //transform.Translate(Vector2.right * moveSpeed * Time.deltaTime); 
+    }
+
+    void FixedUpdate()
+    {
+        //Moves the player in the direction of moveDirection
+        rb.MovePosition(rb.position + _moveDirection * moveSpeed * Time.fixedDeltaTime);
     }
 }
